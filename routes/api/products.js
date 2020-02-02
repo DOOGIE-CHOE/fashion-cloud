43b79b3f4df40dc070c53b7a06c58e0728d388f8
Daniel Choi <daniel.choi@gililab.com>
2020-02-02 18:03:09 +0900

    Add APIs - GET /all - GET /:id - POST /:id

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,25 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const code = require('../../config/code');
+const CacheService = require('../../core/cache');
+
 const Products = mongoose.model('Products');
 const router = express.Router();
+
+const ttl = 60 * 60;
+const cache = new CacheService(ttl);
 
 router.get('/', (req, res) => {
   res.send('hello world');
 });
 
-router.post('/', (req, res) => {
-  const products = new Products(req.body.keys)
-  products.save(function(err){
-    if(err){
-      console.error(err);
-      res.json({result: 0});
-      return;
-    }
-
-    res.json({result: 1});
-
+router.get('/all', (req, res) => {
+  cache.getAll().then((value) => {
+    res.json(value);
   });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  cache.get(id, () => {
+    return new Promise((resolve) => {
+      resolve(crypto.randomBytes(6).toString('hex'));
+    });
+  }).then((value) => {
+    if (value && value.isNew) {
+      res.json(value.key);
+    } else {
+      Products.find({ key: value }).then((result) => {
+        res.json(result);
+      });
+    }
+  });
+});
+
+router.post('/:id', (req, res) => {
+  const { id } = req.params;
+  cache.get(id).then((value) => {
+    const data = req.body;
+    data.key = value;
+    const products = new Products(data);
+    products.save((err) => {
+      if (err) {
+        console.error(err);
+        res.json({ result: 0 });
+      }
+      res.json({ result: 1 });
+    });
+  });
+});
 
 module.exports = router;
